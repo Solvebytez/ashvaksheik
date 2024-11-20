69cd284aac42db14c62ca3a4af4e3a816c4bf2df
Solvebytez <githubsolvebytez@gmail.com>
2024-11-20 17:44:19 +0530

    changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const formatDate = (date:any) => {
-    const options = { year: 'numeric', month: 'short', day: 'numeric' };
-    const formattedDate = new Date(date).toLocaleDateString('en-US', options);
+    // const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    const formattedDate = new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
     return formattedDate.replace(/([A-Za-z]+)/, (month) => month.toUpperCase());
   };
