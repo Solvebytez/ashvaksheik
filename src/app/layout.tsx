0a755a82545583376910564c87d38513684afc8e
Solvebytez <githubsolvebytez@gmail.com>
2025-05-30 11:13:07 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,6 @@ import { Metadata } from "next";
 import { ApolloWrapper } from "./ApolloProvider";
 import Script from "next/script";
 
- {/* Google Tag Manager - Head */}
-   <meta name="google-site-verification" content="afTMMpDhrclb2UVVXLKFnSbbLlaqh1zBgHArdam4PcU" />
 const montserrat = Montserrat({
   subsets: ["latin", "cyrillic", "latin-ext"],
   variable: "--font-montserrat",
@@ -32,6 +30,9 @@ export const metadata: Metadata = {
   description:
     "Serving the Greater Toronto Area (GTA), including Peel, Durham, Halton, York, and other surrounding regions.",
     keywords:["Hyderabadi realtor in Mississauga,  Experienced Hyderabadi realtor in Mississauga, Telugu realtor in Mississauga, Telugu realtor in Brampton,  Best South Indian property consultant in Mississauga, Telugu realtor in Burlington, Telugu realtor in Milton, Telugu realtor in Oakville, Top Telugu realtor in GTA, Best Hyderabadi real estate agent in GTA, Top Telugu realtor in Durham Region, Telugu realtor in Whitby, Hyderabadi real estate agent in Whitby, Telugu realtor in Oshawa"]
+  verification: {
+    google: "afTMMpDhrclb2UVVXLKFnSbbLlaqh1zBgHArdam4PcU",
+  },
 };
 
 export default async function RootLayout({
