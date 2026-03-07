bc21dba1b4fcc04fa026211c0e2e68b33855939b
Solvebytez <githubsolvebytez@gmail.com>
2026-03-07 19:57:32 +0530

    Add Google Tag Manager (GTM-N6J6862H) to header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export default async function RootLayout({
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5PR8C8D7');`,
+          })(window,document,'script','dataLayer','GTM-N6J6862H');`,
           }}
         />
       </head>
@@ -67,12 +67,13 @@ export default async function RootLayout({
           {/* Google Tag Manager (noscript) */}
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-5PR8C8D7"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-N6J6862H"
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
             />
           </noscript>
+          {/* End Google Tag Manager (noscript) */}
           <div className="bg-black ">
             <Header />
             {children}
